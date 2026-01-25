59c4fd8ed120663b3ea00b5e104dd17d0fde39bf
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-01-25 17:27:01 +0200

    Alphanum

---
+++ alphanumber.go
@@ -0,0 +1,31 @@
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("")
+}
+
+func AlphaNumber(n int) string {
+	if n == 0 {
+		return "a"
+	}
+
+	result := ""
+	sign := ""
+
+	if n < 0 {
+		sign = "-"
+		n = -n
+	}
+
+	for n > 0 {
+		digit := n % 10
+		letter := rune('a' + digit)
+		result = string(letter) + result
+		n /= 10
+	}
+
+	return sign + result
+}
