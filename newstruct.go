3434000b336893599893c1d73d78e45cbf974ff4
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-01-31 12:25:03 +0200

    newStruct

--- newstruct.go
+++ newstruct.go
@@ -1,15 +1,17 @@
 package main
 
 
-type poin struct {
-	X float32.
-	Y float32
+
+type Point struct {
+	X    float32
+	Y    float32
 	Text string
 }
-	func makepoint (x, y float32,textstring) point{
-		return point {
-			X: x,
-			Y: y,
-			Text: text
-		}
+
+func MakePoint(x, y float32, text string) Point {
+	return Point{
+		X:    x,
+		Y:    y,
+		Text: text,
 	}
+}
