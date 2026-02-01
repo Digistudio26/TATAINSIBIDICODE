505607758fdd9d1eabad45e6121d1824441c9c00
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-02-01 17:46:39 +0200

    correct pointtext

--- pointtext.go
+++ pointtext.go
@@ -3,19 +3,14 @@ package main
 
 
 type Point struct {
-	X    float64
-	Y    float64
+	X    float32
+	Y    float32
 	Text string
 }
 
 func PointText(p Point) Point {
-	// Convert floats to string with 6 decimal places
-	xStr := strconv.FormatFloat(p.X, 'f', 6, 64)
-	yStr := strconv.FormatFloat(p.Y, 'f', 6, 64)
-
-	return Point{
-		X:    p.X,
-		Y:    p.Y,
-		Text: "Text at (" + xStr + ", " + yStr + ")",
-	}
-}
+	
+		P.text= fmt.Sprintf("Text at (%f, %f)", p.X, p.Y)
+return p
+	
+} 
